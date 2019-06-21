87d6aea7fc4710da010377654f00b999c5079787
Luis Zabala <luis.zabala076@gmail.com>
2019-06-21 14:07:07 -0400

    R_Container

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
-  render() {
-    return (
-      <div>
-        Restaurant Input
-      </div>
-    );
-  }
-};
+
 
 export default RestaurantInput;
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react'
-import RestaurantInput from '../components/restaurants/RestaurantInput'
-import Restaurants from '../components/restaurants/Restaurants'
+import React, { Component } from 'react';
+import RestaurantInput from '../components/restaurants/RestaurantInput';
+import Restaurants from '../components/restaurants/Restaurants';
+import { connect } from 'react-redux';
+
+// <RestaurantInput : set up input props for restaurant, addRestaurant
+// <Restaurants: set up props for restaurant and its actions
+// mapDispatchToProps dispatch actions
 
 class RestaurantsContainer extends Component {
 
   render() {
     return (
-      <div>
-        <RestaurantInput />
-        <Restaurants />
+      <div>      
+        
+        <RestaurantInput 
+          addRestaurant={this.props.addRestaurant} 
+        />
+        
+        <Restaurants 
+          restaurants={this.props.restaurants}
+          deleteRestaurants={this.props.deleteRestaurants}
+        />
+
       </div>
     )
   }
 }
 
-export default RestaurantsContainer
+const mapStateToProps = state => ({ restaurants: state.restaurants });
+
+const mapDispatchToProps = dispatch => ({
+
+  addRestaurant: text => dispatch({ type: 'ADD_RESTAURANT', text}),
+  deleteRestaurant: id => dispatch({type: 'DELETE_RESTAURANT', id})
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer)
